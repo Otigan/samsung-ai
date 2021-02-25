48014182a8bf53079bef67b4333dac48fe3fd6ae
Bubka-Gob <f.skorokhodov@gmail.com>
2021-02-25 16:15:16 +0600

    Test regression

--- calories_dataset.py
+++ calories_dataset.py
@@ -1,5 +1,6 @@
 from database.data_base import Calories, init_base
 import tensorflow as tf
+
 from PIL import Image
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
@@ -28,5 +29,44 @@ def load_data(path):
 
 load_data('C:/Project/Python/pyCharmTest2/Pictures')
 
+images_list = images_list[:128]
+calories_list = calories_list[:128]
+
 print(len(images_list))
 print(len(calories_list))
+
+dataset = tf.data.Dataset.from_tensor_slices((images_list,calories_list)).batch(16)
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(6, 3, activation='selu', padding='same', input_shape=(224,224,3)),
+    tf.keras.layers.Conv2D(6, 3, activation='selu', padding='same'),
+    tf.keras.layers.Conv2D(6, 3, activation='selu', padding='same'),
+    tf.keras.layers.MaxPool2D(2),
+
+    tf.keras.layers.Conv2D(12, 3, activation='selu', padding='same'),
+    tf.keras.layers.Conv2D(12, 3, activation='selu', padding='same'),
+    tf.keras.layers.MaxPool2D(2),
+
+    tf.keras.layers.Conv2D(18, 3, activation='selu', padding='same'),
+    tf.keras.layers.Conv2D(18, 3, activation='selu', padding='same'),
+    tf.keras.layers.MaxPool2D(2),
+
+    tf.keras.layers.Conv2D(24, 3, activation='selu', padding='same'),
+    tf.keras.layers.Conv2D(24, 3, activation='selu', padding='same'),
+    tf.keras.layers.MaxPool2D(2),
+
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(1)
+])
+model.summary()
+model.compile(
+        optimizer=tf.keras.optimizers.Adam(),
+        loss=tf.keras.losses.MeanSquaredError(),
+        metrics=['mae']
+    )
+
+model.fit(dataset, epochs=60)
+
+predicted = model.predict(tf.data.Dataset.from_tensor_slices([images_list[4]]).batch(1))
+print(f'predicted={predicted}   true={calories_list[4]}')
+
