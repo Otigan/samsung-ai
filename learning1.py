76f3e5b5af2093bda52c0cb3612580ba8672a76c
Bubka-Gob <f.skorokhodov@gmail.com>
2021-02-26 19:59:20 +0600

    Stable version for big dataset

--- learning1.py
+++ learning1.py
@@ -2,25 +2,33 @@ import tensorflow as tf
 import os
 from utils.custom_dataset_loader import Loader
 
-loader = Loader(path=f'{os.getcwd()}/Pictures',
-                loading_batch=3200,
-                learning_batch=32,
-                num_epochs=3)
-
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(6, 3, activation='selu', padding='same', input_shape=(224,224,3)),
-    tf.keras.layers.MaxPool2D(16),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(1)
-])
-model.summary()
-model.compile(
+def create_model():
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(6, 3, activation='selu', padding='same', input_shape=(224,224,3)),
+        tf.keras.layers.MaxPool2D(16),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(1)
+    ])
+    model.compile(
         optimizer=tf.keras.optimizers.Adam(),
         loss=tf.keras.losses.MeanSquaredError(),
         metrics=['mae']
     )
+    return model
 
+
+loader = Loader(path=f'{os.getcwd()}/Pictures',
+                batch=1600,
+                validation_split=0.2,
+                num_epochs=20)
+model = create_model()
 while loader.is_going:
-    train_dataset = loader.load_next_data()
-    model.fit(train_dataset, epochs=1)
+    x, y , val_x, val_y= loader.load_next_data()
+    tf.keras.backend.clear_session()
+    model.fit([x],[y], validation_data=(val_x, val_y), batch_size=32, epochs=1) #should be 1 epoch here. Real number of epochs defines by Loader parameters
+    weights = model.get_weights()
+    model = create_model()
+    model.set_weights(weights) # preventing memory leak,  creating new model with old weights
+
+
 
