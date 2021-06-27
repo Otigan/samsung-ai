9a837a046543d3433e8d6fb818e8707aed684ca2
Bubka-Gob <f.skorokhodov@gmail.com>
2021-06-27 19:44:00 +0700

    Final

--- website/site.py
+++ website/site.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, redirect, url_for, request
 from utils import image_resize
 from PIL import Image
+from flask import jsonify
 import os
 import tensorflow as tf
-import json
 
 model = tf.keras.models.load_model('model') # model.save('model')
 web_site = Flask(__name__)
@@ -15,7 +15,6 @@ def home():
     else:
         image = request.files['image']
         image.save(os.path.join('static/uploaded', image.filename))
-
         img = Image.open(os.path.join('static/uploaded', image.filename))
         img = image_resize(img)
         img.save(os.path.join('static/uploaded', image.filename))
@@ -23,15 +22,13 @@ def home():
         img = img / 255
 
         calories = model.predict(tf.data.Dataset.from_tensor_slices([img]).batch(1))
-        print(calories[0][0])
-        return render_template('page.html',calories=str(calories[0][0])+" Калорий", source=url_for('static', filename='uploaded/'+image.filename))
+        return render_template('page.html', calories=str(int(calories[0][0]))+" Килокалорий",
+                               source=url_for('static', filename='uploaded/'+image.filename))
 
 @web_site.route('/mobile', methods= ['POST'])
 def mobile():
-    image = None
     try: image = request.files['image']
-    except: pass
-    print(image)
+    except: return jsonify({'result': 'failure'})
     image.save(os.path.join('static/uploaded', image.filename))
     img = Image.open(os.path.join('static/uploaded', image.filename))
     img = image_resize(img)
@@ -39,9 +36,8 @@ def mobile():
     img = tf.keras.preprocessing.image.img_to_array(img)
     img = img/255
     calories = model.predict(tf.data.Dataset.from_tensor_slices([img]).batch(1))
-    print(calories[0][0])
 
-    to_return = calories[0][0]
-    return json.dumps(to_return)
+    to_return = {'result': 'success', 'calories': int(calories[0][0])}
+    return jsonify(to_return)
 
 web_site.run(debug=True, host='192.168.100.3')
